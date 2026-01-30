80eabbc189fb26fa1081d41d389fee1bebbdd786
DHCuong27 <dangcuong9331@gmail.com>
2026-01-30 14:10:08 +0700

    Fix View

--- FinalCuongFilm.Service/Services/EpisodeService.cs
+++ FinalCuongFilm.Service/Services/EpisodeService.cs
@@ -1,6 +1,5 @@
 ﻿using FinalCuongFilm.ApplicationCore.Entities;
 using FinalCuongFilm.Common.DTOs;
-using FinalCuongFilm.Datalayer;
 using FinalCuongFilm.DataLayer;
 using FinalCuongFilm.Service.Interfaces;
 using Microsoft.EntityFrameworkCore;
